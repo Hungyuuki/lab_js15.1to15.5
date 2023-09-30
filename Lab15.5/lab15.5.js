9a8cc1eebf9e1e0a92f718979929faf4627f1903
Hungyuuki <hungyuuki777@gmail.com>
2023-09-30 15:04:37 +0700

    thuc hanh ep kieu

--- Lab15.5/lab15.5.js
+++ Lab15.5/lab15.5.js
@@ -1,3 +1,4 @@
+// Lab 15.5
 let country = "Việt Nam";
 let population = 80000000;
 let language = "Vietnamese";
@@ -12,3 +13,42 @@ if (population > average) {
   text_3 = `${country}'s population is < 33 - population > million below average`;
   console.log(text_3);
 }
+
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log(" ");
+
+// Thực hành ép kiểu ngầm định, ép dạng số sang dạng string
+console.log("Ép kiểu Implicit 1: ");
+let numb = 100;
+let addString = "Team chúng tôi có " + numb + " người.";
+console.log(
+  addString + " Kiểu dl của numb " + ` ${numb}` + " là " + typeof numb
+);
+
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log(" ");
+
+console.log("Ép kiểu Implicit 2: ");
+let num = 50;
+let str = num + "";
+console.log(str + " Kiểu dl của num là " + typeof str); //ép kiểu thành công
+console.log(num + " Kiểu dl của num là " + typeof numb);
+
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log(" ");
+console.log("Ép kiểu Explicit: ");
+let number = 30;
+let convertNumberToString = String(number);
+console.log(typeof number + " " + typeof convertNumberToString);
