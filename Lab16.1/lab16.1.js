44ea3659cfc32caf2377a3d5fe5930fd3e29566a
Hungyuuki <hungyuuki777@gmail.com>
2023-09-30 15:14:52 +0700

    lab 16.1

---
+++ Lab16.1/lab16.1.js
@@ -0,0 +1,6 @@
+// Lab 16.1
+console.log("9" - "5"); //ra dạng số vì chỉ có phép trừ
+console.log("19" - "13" + "17"); //ra dạng chuỗi vì có cả phép cộng
+console.log("19" - "13" + 17); //ra dạng số
+console.log("123" < 57); //false
+console.log(5 + 6 + "4" + 9 - 4 - 2); //1143
